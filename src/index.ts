7c2326d2bac3a80dea3d469d5ecd5cf84a3e80cd
seu nome <adnanbezerra@gmail.com>
2023-01-10 14:31:42 -0300

    refactor: make everything prettier

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import 'express-async-errors';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import errorHandler from './middlewares/ErrorHandler';
-dotenv.config()
+dotenv.config();
 
 const server = express();
 server.use(cors());
@@ -11,6 +11,6 @@ server.use(express.json());
 
 // routes
 // server.use();
-server.use(errorHandler)
+server.use(errorHandler);
 
 export default server;
