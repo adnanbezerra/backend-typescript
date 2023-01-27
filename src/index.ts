434196d3ce80cfe79d2678c1f6f0f2110d3795a2
seu nome <adnanbezerra@gmail.com>
2023-01-27 19:46:11 -0300

    refactor: remove unused dotenv import

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 import express from 'express';
 import 'express-async-errors';
-import dotenv from 'dotenv';
 import cors from 'cors';
 import errorHandler from './middlewares/ErrorHandler';
-dotenv.config();
 
 const server = express();
 server.use(cors());
